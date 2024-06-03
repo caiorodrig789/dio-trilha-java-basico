494fcda862d09b7659361bf933507b970c8991e8
CAIO <CAIO@CAIO>
2024-06-03 20:34:59 -0300

    commit4

---
+++ Terminal-Argumento/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+
+		System.out.println("Oi Turma, fui executado pelo Terminal");
+	}
+
+} 
+
+//Foi executado pelo cmd esse exercicio !!!
